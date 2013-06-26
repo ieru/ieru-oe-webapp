b81d220efa1db587b3d9f95dde76b687e9093a44
David Baños <maketas@gmail.com>
2013-06-26 15:57:44 +0200

    Added databases

---
+++ ieru_organic_oauth.sql
@@ -0,0 +1,60 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : [DEV] Localhost
+ Source Server Version : 50527
+ Source Host           : localhost
+ Source Database       : ieru_organic_oauth
+
+ Target Server Version : 50527
+ File Encoding         : utf-8
+
+ Date: 06/26/2013 15:55:56 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `tokens`
+-- ----------------------------
+DROP TABLE IF EXISTS `tokens`;
+CREATE TABLE `tokens` (
+  `token_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token_active` tinyint(4) NOT NULL,
+  `token_chars` char(80) NOT NULL,
+  `token_expire` datetime NOT NULL,
+  `token_ip` char(15) NOT NULL,
+  PRIMARY KEY (`token_id`),
+  KEY `user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) DEFAULT NULL,
+  `user_username` varchar(20) NOT NULL DEFAULT '',
+  `user_active` tinyint(1) DEFAULT NULL,
+  `user_password` varchar(100) NOT NULL DEFAULT '',
+  `user_password_joomla` varchar(65) DEFAULT NULL,
+  `user_email` varchar(255) NOT NULL DEFAULT '',
+  `user_creation_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_activation_hash` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`user_username`) USING BTREE,
+  UNIQUE KEY `id` (`user_id`) USING BTREE,
+  UNIQUE KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Records of `users`
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES ('1', 'David BaÃ±os ExpÃ³sito', 'david', '1', 'b86ea3bcb78a2f226a9349fb1a8244e8:70d8dca44f0b52355f560218fbefef6d', null, 'david@teluria.es', '0000-00-00 00:00:00', '');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
