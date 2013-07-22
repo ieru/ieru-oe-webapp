6675122e5271d8f44b70d3089e20e3d88e17573b
David Baños <maketas@gmail.com>
2013-07-22 03:53:18 +0200

    Bug

--- public/js/app.views.js
+++ public/js/app.views.js
@@ -585,7 +585,7 @@ App.Views.FiltersBar = Backbone.View.extend({
         render: function(){
             if ( this.model.get('clave') == 'keyword' )
                 this.$el.addClass('label-info').removeClass('label-success');
-            this.$el.html( '<button id="close-button-'+this.model.get('valor').trim().replace(/ /g, '-')+'" class="close" style="float: none;">&times;</button> <span>'+this.model.get('valor')+'</span>' );
+            this.$el.html( '<button id="close-button-'+this.model.get('valor').trim().replace(/ /g, '-')+'" class="close" style="float: none;">&times;</button> <span>'+this.model.get('valor').replace(/@/g, '-')+'</span>' );
             return this;
         },
     })
