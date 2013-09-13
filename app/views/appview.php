e3f20d48d0820269ffd2af1b3e3b5ea1ff72def1
David Baños <maketas@gmail.com>
2013-09-13 15:16:01 +0200

    Support for backbone.js and google analytics

--- app/views/appview.php
+++ app/views/appview.php
@@ -642,6 +642,14 @@
             </ul>
         </script>
 
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-12236017-1']);
+
+        </script>
+
+
         <!-- jQuery + Bootstrap -->
         <script src="js/jquery.js"></script>
         <script src="js/vendor/bootstrap/bootstrap.js"></script>
@@ -745,18 +753,5 @@
             };
         </script>
 
-        <script type="text/javascript">
-
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-12236017-1']);
-          _gaq.push(['_trackPageview']);
-
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
-
-        </script>
     </body>
 </html>
