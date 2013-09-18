4eda7cca00c28018c2116fcec8336f94f5240efa
David Baños <maketas@gmail.com>
2013-09-18 12:43:23 +0200

    Added new routes + Google webmaster code

--- public/js/app.router.js
+++ public/js/app.router.js
@@ -13,6 +13,9 @@ App.Router = Backbone.Router.extend({
 
 		'navigation': 				 'navigation',
 		'navigation/:id': 			 'navigation',
+
+		'listing': 					 'listing',
+		'listing/:id': 				 'listing'
 	},
 
 	index: function(){
@@ -53,6 +56,10 @@ App.Router = Backbone.Router.extend({
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	},
+
+	listing: function(){
+		show_view( 'page-app' );
 	},
 
 	search: function(text){
