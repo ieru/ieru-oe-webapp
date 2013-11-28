61aa6bd692b1691782a6bff8d5106b3f169bdcfd
David Baños <maketas@gmail.com>
2013-11-28 13:29:24 +0100

    Added metadata statistics

--- app/views/admin/metadatastatistics.blade.php
+++ app/views/admin/metadatastatistics.blade.php
@@ -2,5 +2,6 @@
 
 
 @section('content')
-    <p>In development</p>
+    <iframe src="http://ie3.cc.uah.es/agstats/elements.html" width="100%" height="350" style="border: 0; "></iframe>
+    <iframe src="http://ie3.cc.uah.es/agstats/languages.html" width="100%" height="350" style="border: 0; "></iframe>
 @stop
