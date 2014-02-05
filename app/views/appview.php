79f4fd2047e491940f047c6a6720c0e9f6d0c50b
David Baños <maketas@gmail.com>
2014-02-05 14:46:19 +0100

    Changed versions for forcing reload of files

--- app/views/appview.php
+++ app/views/appview.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="<?php echo LANG ?>">
     <head>
-        <?php define( 'VERSION', '0.2.8' );?>
+        <?php define( 'VERSION', '0.2.9' );?>
         <title>Organic.Edunet</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
